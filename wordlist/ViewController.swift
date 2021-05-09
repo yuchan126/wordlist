931b45fea6e50cd56ffa1c07ff982155f98206c1
Yuma Ikeda <yumaxikeda@gmail.com>
2021-05-09 16:34:50 +0900

    Initial Commit

---
+++ wordlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordlist
+//
+//  Created by Yuma Ikeda on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
